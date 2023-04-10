94553d12b024d331673de18165fdcc9bf3b8b3f0
Muhammad Zaryab Khan <88337527+zaryabkhan864@users.noreply.github.com>
2023-04-10 09:01:40 +0300

    prediction controller updated

--- backend/app.js
+++ backend/app.js
@@ -1,55 +1,9 @@
-// // adding tensorFlow
-// const tf = require('@tensorflow/tfjs');
-
-
-const tf = require('@tensorflow/tfjs-node');
-const XLSX = require('xlsx');
-
 const express = require('express')
 const bodyParser = require('body-parser');
-const path = require('path');
-
-const workbook = XLSX.readFile(path.join(__dirname, '../backend/dataset/heart.xlsx'));
-const worksheet = workbook.Sheets['Sheet1'];
-const data = XLSX.utils.sheet_to_json(worksheet);
-
-// Split the data into training and testing sets
-const splitIndex = Math.round(data.length * 0.8);
-const trainingData = data.slice(0, splitIndex);
-const testingData = data.slice(splitIndex);
-
-// Define the model architecture
-const model = tf.sequential();
-model.add(tf.layers.dense({ inputShape: [5], units: 16, activation: 'relu' }));
-model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
-model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy', metrics: ['accuracy'] });
-
-// Convert the training data to tensors
-const trainingInputs = tf.tensor(trainingData.map(d => [d.age, d.gender === 'male' ? 0 : 1, d.weight, d.bloodPressure, d.heartRate]));
-const trainingLabels = tf.tensor(trainingData.map(d => d.heartAttack));
-
-// Train the model
-model.fit(trainingInputs, trainingLabels, { epochs: 50 }).then(() => {
-    console.log('Model trained');
-});
-
 const app = express()
 const errorMiddleware = require('./middlewares/erros')
 app.use(express.json());
 app.use(bodyParser.json());
-
-// // Define the model architecture
-// const model = tf.sequential();
-// model.add(tf.layers.dense({ units: 10, inputShape: [4], activation: 'relu' }));
-// model.add(tf.layers.dense({ units: 10, activation: 'relu' }));
-// model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
-// model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy' });
-
-// // load the pre-trained weights
-// (async function () {
-//     await model.load('../backend/dataset/heart.xlsx')
-//     console.log('Model loaded 1')
-// })();
 
 //Import all routes
 const predict = require('./routes/bpmRoute')
--- backend/controllers/bpmController.js
+++ backend/controllers/bpmController.js
@@ -1,27 +1,38 @@
-// const Product = require('../models/product')
-
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const APIFeatures = require('../utils/apiFeatures')
+const tf = require('@tensorflow/tfjs-node');
+const XLSX = require('xlsx');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const workbook = XLSX.readFile(path.join(__dirname, '../dataset/heart.xlsx'));
+const worksheet = workbook.Sheets['Sheet1'];
+const data = XLSX.utils.sheet_to_json(worksheet);
 
+// Split the data into training and testing sets
+const splitIndex = Math.round(data.length * 0.8);
+const trainingData = data.slice(0, splitIndex);
+const testingData = data.slice(splitIndex);
+
+// Define the model architecture
+const model = tf.sequential();
+model.add(tf.layers.dense({ inputShape: [5], units: 16, activation: 'relu' }));
+model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
+model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy', metrics: ['accuracy'] });
+
+// Convert the training data to tensors
+const trainingInputs = tf.tensor(trainingData.map(d => [d.age, d.gender === 'male' ? 0 : 1, d.weight, d.bloodPressure, d.heartRate]));
+const trainingLabels = tf.tensor(trainingData.map(d => d.heartAttack));
+
+// Train the model
+model.fit(trainingInputs, trainingLabels, { epochs: 50 }).then(() => {
+    console.log('Model trained');
+});
 
 
 // Get single product details   =>   /api/v1/product/:id
 exports.getPredict = catchAsyncErrors(async (req, res, next) => {
-    // const bpmValueReciving = req.params.id;
-    // console.log(`the bpm value is `, bpmValueReciving);
-    // await model.load('../dataset/heart.xlsx')
-    // console.log('Model loaded 2')
-
-    // const { BP, HR, age, sex } = req.query;
-    // const input = tf.tensor2d([[BP, HR, age, sex]]);
-    // const output = model.predict(input).dataSync()[0];
-    // const isHeartAttack = output >= 0.5 ? 'likely' : 'unlikely';
-    // res.json({
-    //     prediction: isHeartAttack,
-    //     probability: output
-    // });
     const { age, sex, weight, BP, HR } = req.body;
     const input = tf.tensor([[age, sex === 'male' ? 0 : 1, weight, BP, HR]]);
     const prediction = model.predict(input).dataSync()[0];
